0658609898b1c445ecde1de106ab8a406d1ce7fd
Erik Eggers <em.eggers@gmail.com>
2015-02-28 17:55:08 -0500

    Dynamic route for each contact now works

--- app/routes/contacts/show.js
+++ app/routes/contacts/show.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return Ember.$.ajax('https://api.parse.com/1/classes/contact').then(function(response){
-      return response.results;
-    });
+  model: function(params) {
+    return Ember.$.ajax('https://api.parse.com/1/classes/contact/'+params.objectId);
   }
 });
